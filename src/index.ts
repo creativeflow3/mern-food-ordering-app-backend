e85e0066c85f7ff4e72bd337ed8c68a745f74251
Jonathan McNeil <creativeflow3@gmail.com>
2024-06-08 13:37:39 -0700

    first commit, initial api setup

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+import MyUserRoutes from './routes/MyUserRoutes';
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING as string)
+  .then(() => console.log('connected to database!'));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/health', async (req: Request, res: Response) => {
+  res.send({ message: 'health OK!' });
+});
+
+app.use('/api/my/user', MyUserRoutes);
+
+app.listen(7000, () => {
+  console.log('server started on localhost:7000');
+});
